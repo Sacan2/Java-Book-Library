0d1c3e097604a5804c84a135b1b9254aae70d3b9
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-12-18 14:49:35 +0100

    Nur Spring Boot über Gradle installiert nicht besonderes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,28 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "serhad.can"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(23))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+tasks.named<Test>("test") {
     useJUnitPlatform()
 }
---
+++ src/main/java/serhad/can/java_library_book/JavaLibraryBookApplication.java
@@ -0,0 +1,13 @@
+package serhad.can.java_library_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLibraryBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLibraryBookApplication.class, args);
+    }
+
+}
