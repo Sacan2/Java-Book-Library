45460e7f385b6b03971c45b77db4b116a724d9e7
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-12-27 16:19:40 +0100

    nur ein Text

--- src/main/java/serhad/can/java_library_book/JavaLibraryBookApplication.java
+++ src/main/java/serhad/can/java_library_book/JavaLibraryBookApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaLibraryBookApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaLibraryBookApplication.class, args);
+        System.out.println("Spring läuft");
 
         
 
