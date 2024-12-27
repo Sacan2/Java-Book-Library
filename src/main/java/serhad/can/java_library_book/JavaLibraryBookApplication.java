ad3435e0f8994de255c98d217c9d874182cb3953
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2024-12-27 15:55:55 +0100

    Mit spring boot kann ich json request verschicken.

--- src/main/java/serhad/can/java_library_book/JavaLibraryBookApplication.java
+++ src/main/java/serhad/can/java_library_book/JavaLibraryBookApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaLibraryBookApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaLibraryBookApplication.class, args);
-        System.out.println("Test test");
 
         
 
