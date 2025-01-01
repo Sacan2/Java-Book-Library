76cba7bea66076cd59bedd5d309709a41f72be5c
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-01 14:03:12 +0100

    Kleine Änderungen

--- src/main/java/serhad/can/java_library_book/GreetingController.java
+++ src/main/java/serhad/can/java_library_book/GreetingController.java
@@ -1,17 +1,25 @@
 package serhad.can.java_library_book;
 
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, Swagger!";
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Serhad") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/hallo")
+    public String index(){
+        return "KAKA";
     }
 
 }
--- src/main/java/serhad/can/java_library_book/SwaggerConfig.java
+++ src/main/java/serhad/can/java_library_book/SwaggerConfig.java
@@ -1,10 +1,20 @@
 package serhad.can.java_library_book;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+@RestController
+@RequestMapping("/api")
 public class SwaggerConfig {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Swagger!";
+    }
+
+
 
 }
 
