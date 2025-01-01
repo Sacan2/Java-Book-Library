e92ef0157905048e93cabd7e6318c5aa1864c9c4
Sacan2 <93250860+Sacan2@users.noreply.github.com>
2025-01-01 13:47:48 +0100

    Swagger UI wurde eingefügt wird unter der Seite http://localhost:8080/swagger-ui/index.html#/ aufgerufen im Browser. Ich hatte im Graddle die version 23 am laufen dabei gibt es aktuell nur die Java version 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(23))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
@@ -19,9 +19,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0") // Neueste Version verwenden
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
---
+++ src/main/java/serhad/can/java_library_book/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package serhad.can.java_library_book;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+}
+
